2ffc60f211cbd95181ba461c6287af24dd7ef10f
nicklaros <nicklaros@gmail.com>
2026-08-20 06:19:32 +0000

    fix response

--- app/src/main/java/com/jalanrusak/data/api/ApiClient.kt
+++ app/src/main/java/com/jalanrusak/data/api/ApiClient.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
